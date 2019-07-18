ca139aaa2266e12d569499f566a17903a930a634
Maelen <10725805+maelen@users.noreply.github.com>
2019-07-18 19:59:44 -0400

    First somewhat usable version of the script. Notes are correctly ordered as well as other meta events. Markers are implemented and working. Need to fix the tempo. Lines are commented out for the moment.

--- xml2casiomidi.py
+++ xml2casiomidi.py
@@ -1,22 +1,21 @@
 from __future__ import division
-from mido import Message, MetaMessage, MidiFile, MidiTrack
+from mido import Message, MetaMessage, MidiFile, MidiTrack, bpm2tempo
 from lxml import etree
 from io import StringIO, BytesIO
 from sortedcontainers import SortedDict
 
 notes_base_value = {'C':12,'D':14,'E':16,'F':17,'G':19,'A':21,'B':23}
 
+division = 768
 track = MidiTrack()
-sd = SortedDict()
+sd = []
 tree = etree.parse("S03-05.Moonlight_Sonata.xml")
 part = tree.xpath('/score-partwise/part')
 numerator = 4
 denominator = 4
-staves_tick = [0,0]
 event_start = [0,0]
-event_stop = [0,0]
 rest_duration = [0,0]
-division = 768
+tick=[0,0]
 for measure in part[0].xpath("measure"):
     #measure_tick = 
     attributes = measure.find('attributes')
@@ -26,41 +25,36 @@ for measure in part[0].xpath("measure"):
             numerator = int(time.find('beats').text)
             denominator = int(time.find('beat-type').text)
  
-            sd["{:020d}-time_signature-{}".format(staves_tick[0],numerator)] = \
-                MetaMessage('time_signature',
-                            numerator=numerator,
-                            denominator=denominator,
-                            clocks_per_click=250,
-                            notated_32nd_notes_per_beat=8)
+            sd.append({'tick':tick[0], 'event':MetaMessage('time_signature',
+                      numerator=numerator,
+                      denominator=denominator)})
         staves = attributes.find('staves')
         if staves is not None:
             staves = int(staves.text)
     direction = measure.find('direction')
     if direction is not None:
+        #sound = direction.find('sound')
+        #tempo = float(sound.get('tempo')) if sound is not None else 120
+        #sd.append({'tick':tick[0], 'event':MetaMessage('set_tempo', tempo=bpm2tempo(tempo))})
         if  direction.xpath('direction-type/rehearsal'):
-            sd["{:020d}-marker".format(staves_tick[0])] = \
-                MetaMessage('marker', text="0")
+            sd.append({'tick':tick[0], 'event':MetaMessage('marker', text="0")})
         
     for note in measure.xpath("note"):
         rest = note.find('rest')
         pitch = note.find('pitch')
+        chord = note.find('chord')        
+        staff = note.find('staff')
         duration = note.find("duration")
         duration = int(duration.text) if duration is not None else 0
-        chord = note.find('chord')
-        
-        staff = note.find('staff')
         if staff is not None:
             staff = int(staff.text) - 1
 
         if chord is None:
-            event_start[staff] = staves_tick[staff]
-            staves_tick[staff] += duration//24
-            event_stop[staff] = staves_tick[staff]-1 #Append stop to list test if next note start after
+            event_start[staff] = tick[staff]
+            tick[staff] += duration
 
         if rest is not None:
-            staves_tick[staff] += duration//24
-            rest_duration[staff]+=duration//24
-            print("Rest {} {}".format(staff, rest_duration[staff]))
+            tick[staff] += duration
 
         elif pitch is not None:
             step = pitch.find('step')
@@ -70,21 +64,23 @@ for measure in part[0].xpath("measure"):
             octave = pitch.find('octave')
             octave = int(octave.text) if octave is not None else 0
             midi_value = octave * 12 + notes_base_value[step] + alter
-            sd["{:020d}-note_on-{}".format(event_start[staff],midi_value)] = Message('note_on',
-                                                   channel=3 if staff == 0 else 2,
-                                                   note=midi_value,
-                                                   velocity=80,
-                                                   time=rest_duration[staff]+1)
-            rest_duration[staff]=0
-            sd["{:020d}-note_off-{}".format(event_stop[staff],midi_value)] = Message('note_off',
-                                                   channel=3 if staff == 0 else 2,
-                                                   note=midi_value,
-                                                   velocity=80,
-                                                   time=duration)
-            
-mid = MidiFile(type=0, ticks_per_beat=division)
+            sd.append({'tick':event_start[staff], 'event':Message('note_on',
+                              channel=3 if staff == 0 else 2,
+                              note=midi_value,
+                              velocity=80,
+                              time=0)})
+            sd.append({'tick':tick[staff],
+                       'event': Message('note_off',
+                                        channel=3 if staff == 0 else 2,
+                                        note=midi_value,
+                                        velocity=80,
+                                        time=duration)})
+
+mid = MidiFile(type=0, ticks_per_beat=division)            
 mid.tracks.append(track)
-for key, event in sd.items():
-    print('E {}:{}'.format(key,event))
-    track.append(event)        
+previous_tick = 0
+for entry in sorted(sd, key = lambda i: i['tick']) :
+    print('E {}:{}'.format(entry['tick'],entry['event']))
+    track.append(entry['event'].copy(time=entry['tick']-previous_tick))
+    previous_tick = entry['tick']
 mid.save('new_song.mid')
